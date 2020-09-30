a9095d25106de2b4beb870f6a80600481833751b
shivarad <shiva.rrad@gmail.com>
2020-09-30 19:02:37 +0330

    Updates

---
+++ precache-manifest.acc4b01f412e0393affff4d56c936ba3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8c420b35d3d74a626c595e2aee3affc",
+    "url": "/KittyGallery/index.html"
+  },
+  {
+    "revision": "bf24205c65c6dffbe5b5",
+    "url": "/KittyGallery/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "ffe6c13eda23d6c25f4f",
+    "url": "/KittyGallery/static/css/main.6911d0db.chunk.css"
+  },
+  {
+    "revision": "bf24205c65c6dffbe5b5",
+    "url": "/KittyGallery/static/js/2.780851c0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/KittyGallery/static/js/2.780851c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ffe6c13eda23d6c25f4f",
+    "url": "/KittyGallery/static/js/main.4042bc2e.chunk.js"
+  },
+  {
+    "revision": "5060ed868a21660a1a00",
+    "url": "/KittyGallery/static/js/runtime-main.f32403c9.js"
+  },
+  {
+    "revision": "f0d91b088780334cb367ea8a43e70540",
+    "url": "/KittyGallery/static/media/Adventure Time Logo.f0d91b08.woff"
+  }
+]);
